ebdff5321a90d83f8aa34bd1c0073252f5b3e238
jalehbeiraghy <139043273+jalehbeiraghy@users.noreply.github.com>
2023-08-08 02:21:27 +0330

    c

---
+++ pages/dashboard_t.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const dashboard_t = () => {
+  return (
+    <div>dashboard_t</div>
+  )
+}
+
+export default dashboard_t
--- pages/dashbpard_p.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const dashbpard_p = () => {
-  return (
-    <div>dashbpard_p</div>
-  )
-}
